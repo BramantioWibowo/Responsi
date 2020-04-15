f3468e3af69fab95a44f5fa11fc3c034710e3e3d
BramantioWibowo <61927501+BramantioWibowo@users.noreply.github.com>
2020-04-15 15:01:59 +0700

    Add files via upload

---
+++ ResponsiPemrograman2911.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ResponsiPemrograman2911
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Karyawan karyawan1 = new Karyawan(12345, "Ucok", 300000);
+            Karyawan karyawan2 = new Karyawan(67891, "Subejo", 250000);
+
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan1.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine("\n\n Asyiiik kenaikan gaji 10%");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan1.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int gajiBulanan { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if(gaji < 0)
+            {
+                this.gajiBulanan = 0;
+            } else
+            {
+                this.gajiBulanan = gaji;
+            }
+        }
+
+        public void showKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            double tmp = 0;
+            tmp =  0.1 * gajiBulanan;
+            gajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+    }
+}
